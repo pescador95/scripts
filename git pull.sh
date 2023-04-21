e89c60840233526d77ef24507c1d419544b2df25
Iédio João Carabolante Júnior <61909061+pescador95@users.noreply.github.com>
2023-04-21 13:18:32 -0300

    Initial commit
    My scripts release on 2023-04-21.
    Happy birthday to me!

---
+++ git pull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+remote=$(git config --get remote.origin.url)
+if [[ "$remote" =~ .*github\.com.* ]]; then
+    remote_branch="main"
+else
+    remote_branch="master"
+fi
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    gnome-terminal -e "bash -c 'git pull;git switch $remote_branch;git pull;git switch $branch;exec bash'"
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    start bash -c "git pull;git switch $remote_branch;git pull;git switch $branch;exec bash"
+fi
---
+++ git rebase.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+current_path=$(dirname "$0")
+
+cd "$current_path"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    terminal="gnome-terminal"
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    terminal="start"
+fi
+
+remote=$(git config --get remote.origin.url)
+if [[ "$remote" =~ .*github\.com.* ]]; then
+    remote_branch="main"
+else
+    remote_branch="master"
+fi
+
+current_branch=$(git branch --show-current)
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    $terminal -e "bash -c 'git fetch origin;git checkout $remote_branch;git pull origin $remote_branch;git checkout $current_branch;git rebase $remote_branch;exec bash'"
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    $terminal bash -c "git fetch origin;git checkout $remote_branch;git pull origin $remote_branch;git checkout $current_branch;git rebase $remote_branch;cmd /K"
+fi
